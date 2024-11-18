7b32b27bc1e2ed95b91a131a92f132c9285b2b97
ashwinn-si <ashwin23cse@gmail.com>
2024-11-19 00:12:06 +0530

    host bug commit-2

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ let GameID = 0;
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
-
+app.set("views", path.join(__dirname, "views"));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
